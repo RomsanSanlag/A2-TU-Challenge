108dd17f4efedf7da5957f5b15cd9091b4eb6097
Romsan <santinoromero77400@gmail.com>
2023-01-06 14:25:04 +0100

    modif de MathImplementations et StringImplemention

--- TU Challenge/MyMathImplementation.cs
+++ TU Challenge/MyMathImplementation.cs
@@ -1,6 +1,8 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Diagnostics.CodeAnalysis;
 using System.Linq;
+using System.Net.Http.Headers;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -8,6 +10,158 @@ namespace TU_Challenge
 {
     public class MyMathImplementation
     {
-        
+        public static int Add(int a, int b)
+        {
+            return a + b;
+        }
+
+        public static List<int> GenericSort(List<int> toSort, Func<int, int, int> isInOrder)
+        {
+            throw new NotImplementedException();
+        }
+
+        public static List<int> GetAllPrimary(int a)
+        {
+            List<int> NbPrimary = new List<int>();
+            while (a != 0)
+            {
+                if (IsPrimary(a))
+                {
+                    NbPrimary.Add(a);
+                }
+                a--;
+            }
+            return NbPrimary;
+        }
+
+        public static bool IsDivisible(int a, int b)
+        {
+            int c = a % b;
+            if (c == 0)
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            } 
+        }
+
+        public static bool IsEven(int a)
+        {
+            return (a % 2 == 0);
+        }
+
+        public static int IsInOrder(int a, int b)
+        {
+            if (a<b)
+            {
+                return 1;
+            }
+            else if (a>b)
+            {
+                return -1;
+            }
+            else return 0;
+        }
+
+        public static int IsInOrderDesc(int arg1, int arg2)
+        {
+            throw new NotImplementedException();
+        }
+
+        public static bool IsListInOrder(List<int> list)
+        {
+            bool isInOrder = false;
+            int TmpElement = list.ElementAt(0);
+            for (int i = 0; i < list.Count; i++)
+            {
+                int Elem = list.ElementAt(i);
+                if (Elem >= TmpElement)
+                {
+                    isInOrder = true;
+                }
+                else return false;
+            }
+            return isInOrder;
+        }
+
+        public static bool IsMajeur(int age)
+        {
+            if (age < 0) throw new ArgumentException();
+            if (age >= 150) throw new ArgumentException();
+            if (age < 18)
+            {
+                return false;
+            }
+            else
+            {
+                return true;
+            }
+        }
+
+        public static bool IsPrimary(int a)
+        {
+            int i = 2;
+            int repetition = 0;
+            while (a % i !=0)
+            {
+                repetition++;
+                i++;
+            }
+            if(repetition==(a-2))
+            {
+                return true;
+            }
+            else return false;
+        }
+
+        public static int Power(int a, int b)
+        {
+            if (b != 0)
+            {
+                int nbTemp = a;
+                for (int i = 1; i < b; i++)
+                {
+                    nbTemp *= a;
+                }
+                return nbTemp;
+            }
+            else return 1;
+        }
+
+        public static int Power2(int a)
+        {
+            return a * a;
+        }
+
+        public static List<int> Sort(List<int> toSort)
+        {
+            throw new NotImplementedException();
+            //toSort.RemoveAt
+            //toSort.Insert
+            /*
+            int TmpElement = toSort.ElementAt(0);
+            int iterator = 1;
+            bool isSort = false;
+            while (isSort == false)
+            {
+                int Elem = toSort.ElementAt(iterator);
+                if ((Elem == TmpElement) || (Elem > TmpElement))
+                {
+                    Elem = TmpElement;
+                    iterator++;
+                }
+                else if (Elem < TmpElement)
+                {
+                    int ElemToAdd = Elem;
+                    toSort.RemoveAt(iterator);
+                    toSort.Insert((iterator - 1), ElemToAdd);
+                    iterator++;
+                }
+                isSort = IsListInOrder(toSort);
+            }
+            return toSort;*/
+        }
     }
 }
--- TU Challenge/MyStringImplementation.cs
+++ TU Challenge/MyStringImplementation.cs
@@ -8,6 +8,121 @@ namespace TU_Challenge
 {
     public class MyStringImplementation
     {
-        
+        public static string BazardString(string input)
+        {
+            throw new NotImplementedException();
+        }
+
+        public static bool IsNullEmptyOrWhiteSpace(string input)
+        {
+            if (input == null)
+            {
+                return true;
+            }
+            int length = input.Length;
+            for (int i = 0; i < length; i++)
+            {
+                char c = input[i];
+                if ((c >= 'a') && (c <= 'z'))
+                {
+                    return false;
+                }
+            }
+            return true;
+        }
+
+        public static string MixString(string a, string b)
+        {
+            string stringToReturn = "";
+            int testLengthA = a.Length;
+            int testLengthB = b.Length;
+            int conditionFor = 0;
+
+            if ((testLengthA > testLengthB) || testLengthA == testLengthB)
+            {
+                conditionFor = testLengthA;
+            }
+            else conditionFor = testLengthB;
+            
+            for(int i = 0; i < conditionFor; i++)
+            {
+                if (i <= testLengthB)
+                {
+                    char string1 = a[i];
+                    stringToReturn += string1;
+                    a.Remove(i);
+                }
+                    
+                if (i <= testLengthB)
+                {
+                    char string2 = b[i];
+                    stringToReturn += string2;
+                    b.Remove(i);
+                }
+
+                //tester les strings pour voir si elles sont vides avant de refaire un tour
+/*                if (IsNullEmptyOrWhiteSpace(a))
+                {
+                    stringToReturn += b;
+                    break;
+                }
+                else if (IsNullEmptyOrWhiteSpace(b))
+                {
+                    stringToReturn += a;
+                    break;
+                }*/
+                
+            }
+            return stringToReturn;
+        }
+
+        public static string ToCesarCode(string input, int offset)
+        {
+            throw new NotImplementedException();
+        }
+
+        public static string ToLowerCase(string a)
+        {
+            string result = "";
+            for (int i = 0; i < a.Length; i++)
+            {
+                int asciiNb = (int)a[i];
+                if ((asciiNb >= 65 && asciiNb <= 90)||
+                    (asciiNb >= 192 && asciiNb <= 214)||
+                    (asciiNb >= 216 && asciiNb <= 222))
+                {
+                    asciiNb += 32;
+                }
+                result += (char)asciiNb;
+            }
+            return result;
+        }
+
+        public static string UnBazardString(string input)
+        {
+            throw new NotImplementedException();
+        }
+
+        public static string Voyelles(string a)
+        {
+            string tmp = ToLowerCase(a);
+            string voyelles = "aeiou";
+            string toReturn = "";
+            int lengthTmp = tmp.Length;
+            int lengthVoyelles = voyelles.Length;
+            for (int i = 0; i < lengthTmp; i++)
+            {
+                char c = tmp[i];
+                for (int j = 0; i < lengthVoyelles; i++)
+                {
+                    if (c == voyelles[j])
+                    {
+                        toReturn += c;
+                        break;
+                    }
+                }
+            }
+            return toReturn;
+        }
     }
 }
